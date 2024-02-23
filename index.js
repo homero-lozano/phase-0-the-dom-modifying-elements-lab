b4db34302d21347f138e297fa7c285a434393cab
Nico Lozano <homeronicolaslozano@gmail.com>
2024-02-23 14:57:50 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+document.querySelector("main").remove() 
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Nico is the champion"
+
+ 
+
+
+
+
+
+
+
+
+document.body.append(newHeader); 
