3b53f021bc597cd9cc50c107fb70e13a25ea5d3e
Homero Lozano <homeronicolaslozano@gmail.com>
2024-04-11 07:31:01 -0400

    The DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-document.querySelector("main").remove() 
-const newHeader = document.createElement("h1");
-newHeader.id = "victory"
-newHeader.innerHTML = "Nico is the champion"
-
- 
-
-
-
-
-
-
-
-
-document.body.append(newHeader); 
+document.querySelector('main').remove() 
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory' 
+newHeader.innerHTML = 'Nico is the champion' 
